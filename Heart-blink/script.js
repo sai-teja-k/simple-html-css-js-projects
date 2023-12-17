034894a8e42896e54a3a7871fdccea5b3b7125bc
sai-teka-k <saiteja02k@gmail.com>
2023-12-18 00:57:55 +0530

    added script for heartbears seconds and cycle

--- Heart-blink/script.js
+++ Heart-blink/script.js
@@ -1,17 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let beats = 0;
+
     const heart = document.querySelector(".heart");
     const count = document.querySelector(".count");
+    const second = document.querySelector(".sec");
+    const cycles = document.querySelector(".cycle")
+
+    let beats = 0;
+    let sec = 0;
+    let cycle = 0;
 
     function blink() {
+
         beats++;
 
         count.textContent = `HeartBeats : ${beats}`;
+        cycles.textContent = `Cycles : ${cycle}`;
 
-        if(beats == 72) {
-            clearInterval(interval);
+        if(beats === 72) {
+            cycle++;
+            beats = 0;
+            return;
         }
     }
 
-    const interval = setInterval(blink, 833);
+    const intervalDuration = 60000 / 72;
+
+    const interval = setInterval(blink, intervalDuration);
+
+    setInterval( () => {
+        sec++;
+        second.textContent = `Seconds : ${sec}`;
+        if(sec === 60 )
+            sec = 0;
+    }, 1000);
+
 });
